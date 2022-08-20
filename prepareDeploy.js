10ba7b4ac831dbf258fcd393f9fef139e41484eb
Sondre Kjempekjenn <sondre.kjempekjenn@gmail.com>
2022-08-20 18:35:48 +0200

    prepare for deploy script

---
+++ prepareDeploy.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+const timestamp = Date.now();
+const targetPath = `./out/${timestamp}`;
+const tmpPath = path.join(__dirname, `${timestamp}`);
+
+console.info('Moving to temp directories');
+fs.readdirSync('./out');
+fs.renameSync(`./out`, tmpPath);
+
+console.info('Moving to release folder and creating symlink');
+fs.mkdirSync(targetPath, { recursive: true });
+fs.renameSync(`${timestamp}`, targetPath);
+fs.symlinkSync(path.join(__dirname, targetPath), path.join(__dirname, 'out/current'));
