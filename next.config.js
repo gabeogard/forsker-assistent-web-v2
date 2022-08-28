636a42075d20ce17e7ee25810f18891183ab696b
Sondre Kjempekjenn <sondre.kjempekjenn@gmail.com>
2022-08-28 14:49:56 +0200

    target linux musl

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-
-  // Not supported for static HTML export
-  experimental: {
-    images: { unoptimized: true },
-  }
 }
 
 module.exports = nextConfig;
